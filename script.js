0a75a8506483e779ac09a01e236c2f8f99bb2fd2
i-brkic <184224468+i-brkic@users.noreply.github.com>
2025-03-15 12:55:03 +0100

    Display books in myLibrary array
    Display books in myLibrary array and prevent duplicates by checking if the book with the specific id already exists in DOM.

--- script.js
+++ script.js
@@ -1,22 +1,4 @@
-// array filled so that the results can be seen
-const myLibrary = [
-  {
-    name: "El túnel",
-    author: "Ernesto Sábato",
-    releaseDate: "1948",
-  },
-
-  {
-    name: "Sobre héroes y tumbas",
-    author: "Ernesto Sábato",
-    releaseDate: "1961",
-  },
-  {
-    name: "Abaddón el exterminador",
-    author: "Ernesto Sábato",
-    releaseDate: "1974",
-  },
-];
+const myLibrary = [];
 
 function Book(name, author, releaseDate) {
   this.name = name;
@@ -25,27 +7,44 @@ function Book(name, author, releaseDate) {
   this.id = crypto.randomUUID();
 }
 
-const body = document.querySelector("body");
+const list = document.querySelector(".book-list");
 
 function addBookToLibrary() {
   myLibrary.forEach(book => {
-    const bookFrame = document.createElement("div");
-    bookFrame.innerText = `
-    Name: ${book.name} 
-    Author: ${book.author} 
-    Released: ${book.releaseDate}`;
-    bookFrame.classList.add("book");
-    body.appendChild(bookFrame);
+    if (!document.getElementById(book.id)) {
+      const nesto = document.getElementById(book.id);
+      console.log(nesto);
+      const bookFrame = document.createElement("div");
+      bookFrame.innerText = `
+      Name: ${book.name} 
+      Author: ${book.author} 
+      Released: ${book.releaseDate}`;
+      bookFrame.classList.add("book");
+      bookFrame.id = book.id;
+      list.appendChild(bookFrame);
+    }
   });
 }
-
-// addBookToLibrary();
-
-//displaying the form after button press
 
 const button = document.querySelector(".button");
 const form = document.querySelector(".form");
 
 button.addEventListener("click", function () {
   form.classList.toggle("hidden");
+});
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const BookData = new Book(
+    form.name.value,
+    form.author.value,
+    form.releaseYear.value
+  );
+
+  myLibrary.push(BookData);
+
+  addBookToLibrary();
+
+  form.reset();
 });
